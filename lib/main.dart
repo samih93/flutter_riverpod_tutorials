36ceffea36b6e1892141a0e98a5fd08e1d6ccf08
samih93 <samih_damaj@hotmail.com>
2024-06-28 12:36:21 +0300

    cosumer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:youtube_riverpod_tutorials/state_provider/state_provider_example%202/state_provider_example2.dart';
+import 'package:youtube_riverpod_tutorials/change_notifier_provider/change_notifier_provider_example.dart';
+import 'package:youtube_riverpod_tutorials/consumer_widget_consumer_stateful_widget/consumer_statefull_widget_example.dart';
+import 'package:youtube_riverpod_tutorials/consumer_widget_consumer_stateful_widget/cosumer_widget_example.dart';
+import 'package:youtube_riverpod_tutorials/home_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: StateProviderExample2(),
+      home: const ChangeNotifierProviderExample(),
     );
   }
 }
